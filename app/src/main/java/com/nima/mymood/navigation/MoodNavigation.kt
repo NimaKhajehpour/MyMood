de678593df372aee6dc888d835edd0444a574817
NimaKhajehpour <nima1038khajehpour@gmail.com>
2023-07-27 19:09:32 +0430

    Misc Changes

--- app/src/main/java/com/nima/mymood/navigation/MoodNavigation.kt
+++ app/src/main/java/com/nima/mymood/navigation/MoodNavigation.kt
@@ -1,8 +1,13 @@
 package com.nima.mymood.navigation
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -15,77 +20,86 @@ fun MoodNavigation() {
 
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = Screens.HomeScreen.name){
-        composable(Screens.HomeScreen.name){
+    NavHost(navController = navController, startDestination = Screens.HomeScreen.name) {
+        composable(Screens.HomeScreen.name) {
             HomeScreen(navController = navController, viewModel = hiltViewModel())
         }
 
-        composable(Screens.TodayMoodScreen.name+"/{id}",
+        composable(
+            Screens.TodayMoodScreen.name + "/{id}",
             arguments = listOf(
-                navArgument(name = "id"){type = NavType.StringType},
+                navArgument(name = "id") { type = NavType.StringType },
             )
-        ){
-            TodayMoodScreen(navController = navController,
+        ) {
+            TodayMoodScreen(
+                navController = navController,
                 viewModel = hiltViewModel(),
                 id = it.arguments?.getString("id"),
             )
         }
 
-        composable(Screens.MenuScreen.name){
+        composable(Screens.MenuScreen.name) {
             MenuScreen(navController = navController, viewModel = hiltViewModel())
         }
 
-        composable(Screens.HappyEffects.name){
+        composable(Screens.HappyEffects.name) {
             HappyEffectsScreen(navController = navController, viewModel = hiltViewModel())
         }
-        composable(Screens.NeutralEffects.name){
+        composable(Screens.NeutralEffects.name) {
             NeutralEffectsScreen(navController = navController, viewModel = hiltViewModel())
         }
-        composable(Screens.SadEffects.name){
+        composable(Screens.SadEffects.name) {
             SadEffectsScreen(navController = navController, viewModel = hiltViewModel())
         }
 
-        composable(Screens.SavedDays.name){
+        composable(Screens.SavedDays.name) {
             SaveDaysScreen(navController = navController, viewModel = hiltViewModel())
         }
 
-        composable(Screens.DayScreen.name+"/{id}",
+        composable(Screens.DayScreen.name + "/{id}",
             arguments = listOf(
-                navArgument(name = "id"){type = NavType.StringType}
+                navArgument(name = "id") { type = NavType.StringType }
             )
-        ){
-            DayScreen(navController = navController, viewModel = hiltViewModel(),
+        ) {
+            DayScreen(
+                navController = navController, viewModel = hiltViewModel(),
                 id = it.arguments?.getString("id")
             )
         }
 
-        composable(Screens.AboutScreen.name){
+        composable(Screens.AboutScreen.name) {
             AboutScreen(navController = navController)
         }
 
-        composable(Screens.DayGraphScreen.name+"/{id}",
+        composable(Screens.DayGraphScreen.name + "/{id}",
             arguments = listOf(
-                navArgument(name = "id"){type = NavType.StringType}
+                navArgument(name = "id") { type = NavType.StringType }
             )
-        ){
-            DayGraphScreen(navController = navController,
+        ) {
+            DayGraphScreen(
+                navController = navController,
                 id = it.arguments?.getString("id"),
                 hiltViewModel()
             )
         }
 
-        composable(Screens.DonateScreen.name){
+        composable(Screens.DonateScreen.name) {
             DonateScreen(
                 navController = navController
             )
         }
 
-        composable(Screens.EditScreen.name+"/{id}",
+        composable(Screens.EditScreen.name + "/{id}",
             arguments = listOf(
-                navArgument(name = "id"){type = NavType.StringType}
+                navArgument(name = "id") { type = NavType.StringType }
             )
-        ){
-            EditScreen(navController = navController, id = it.arguments?.getString("id"), viewModel = hiltViewModel())
+        ) {
+            EditScreen(
+                navController = navController,
+                id = it.arguments?.getString("id"),
+                viewModel = hiltViewModel()
+            )
+
         }
     }
 }
