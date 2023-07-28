e4a394bc3bc01e10abf6fe1ade12ca64eeb9292d
NimaKhajehpour <nima1038khajehpour@gmail.com>
2023-07-28 23:38:07 +0430

    Misc Changes

--- app/src/main/java/com/nima/mymood/screens/HomeScreen.kt
+++ app/src/main/java/com/nima/mymood/screens/HomeScreen.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavController
 import com.nima.mymood.R
 import com.nima.mymood.components.EffectsListItem
